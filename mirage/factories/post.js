9e88dca5a9f235b862ebb3b251e093d54c8b45e1
Rick Thornbrugh <Rick Thornbrugh>
2016-11-08 11:48:49 -0800

    syntax correction.

--- mirage/factories/post.js
+++ mirage/factories/post.js
@@ -2,7 +2,7 @@ import { Factory } from 'ember-cli-mirage';
 
 export default Factory.extend({
   title(i) {
-    return `Post ${i+1}`
+    return `Post ${i+1}`;
   },
   body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 });
